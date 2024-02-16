004647ac6e51d41661ed503a2b318dd0cf541211
shamshodbekweb <nazarboevshamshodbek@gmail.com^C>
2024-02-16 22:18:54 +0500

    js phrases

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var elInp = document.querySelector(".inp")
+function fn(){
+       var inp = elInp.value
+       if(inp[0] == inp[0].trim()){
+              console.log("to'g'ri");
+       } else{
+              console.log("xato");
+       } 
+}
