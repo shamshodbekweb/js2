14074feba4c8f6a1d9093603896c8adda76936da
shamshodbekweb <159898909+shamshodbekweb@users.noreply.github.com>
2025-06-18 12:25:06 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var elInp = document.querySelector(".inp")
 function fn(){
        var inp = elInp.value
        if(inp[0] == inp[0].trim()){
-              console.log("to'g'ri");
+              console.log("axsdds");
        } else{
               console.log("xato");
        } 
